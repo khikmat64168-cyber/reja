d0e0b4554f072a02f16caf65ffb2036709b19075
abcd <abcd@abcdui-MacBookPro.local>
2026-04-22 15:34:50 +0900

    feat: develop author router ejs  page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('Web serverni boshlash ');
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile('database/user.json', 'utf-8', (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 //1-BOSQICH Kirish codelari expressga kirib kelayotgan malumotlarga bogg'liq bo'lgan kodd yoziladi
 app.use(express.static('public')); //public papkasidagi fayllarni ochish uchun
@@ -19,6 +29,11 @@ app.post('/create-item', (req, res) => {
   console.log(req.body);
   res.json({ test: 'success' });
 });
+
+app.get('/author' , (req, res) => {
+  res.render('author', { user: user });
+});
+
 //  rooterlarga mo'ljallangan
 
 // app.get('/hello', function (req, res) {

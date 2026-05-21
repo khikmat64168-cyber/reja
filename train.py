7512ad57cfdfef14b2cd54f10d49e9d3baa6b7c3
matt <khikmat64168@gmail.com>
2026-05-22 06:38:07 +0900

    feat: L taskni qildim

--- train.py
+++ train.py
@@ -1,3 +1,25 @@
+########### L-TASK ##############
+
+def reverseSentence(gapp):
+  sozlar = gapp.split(" ")
+  reversed_sozlar = [word[::-1] for word in sozlar]
+  return " ".join(reversed_sozlar)
+
+
+print(reverseSentence("we like coding!"))
+print(reverseSentence("anduril completed the target "))
+
+
+
+
+
+
+
+
+
+
+
+'''
 def countVowels(string):
   count = 0
   for harf in string:
@@ -10,3 +32,5 @@ def countVowels(string):
 print(countVowels("string"))    
 print(countVowels("Salom"))    
 print(countVowels("Miiiiiiiiit"))    
+
+'''
